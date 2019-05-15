6cf6c421ca32958d534910d505f9c9acf83bfb80
MYIIIKETEP <Denis.jakusjev@gmail.com>
2019-05-16 00:43:10 +0200

    smthg w word

--- Lektion8/partials/dbcon.php
+++ Lektion8/partials/dbcon.php
@@ -1,6 +1,6 @@
 <?php
   $host = 'localhost';
-  $db   = 'journal';
+  $db   = 'thestore';
   $user = 'root';
   $pass = '';
   $charset = 'utf8';
--- wordpress-5.2/wordpress/wp-content/themes/childSeventeen/function.php
+++ wordpress-5.2/wordpress/wp-content/themes/childSeventeen/function.php
@@ -1,7 +1,10 @@
 <?php
 /* This is a action hook, it tells wordpress to run the funtion below
  * at a specific time (during `wp_enqueue_scripts`). It adds the necessary 
- * CSS to the '<head>'-tag. Noneed to edit this code */
+ * CSS to the '<head>'-tag. Noneed to edit this code 
+ * 
+ * */
+//Этот файл нужен для того чтобы подключить файлы темы
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 
 function my_theme_enqueue_styles() {
@@ -13,4 +16,11 @@ function my_theme_enqueue_styles() {
         array( $parent_style ),
         wp_get_theme()->get('Version')
     );
-}
+    wp_enqueue_style( 'bootstrap-style', 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css');
+
+    // Scripts
+    wp_enqueue_script( 'popper-js', 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js');
+    wp_enqueue_script( 'bootstrap-js', 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js');
+  }
+  add_action( 'wp_enqueue_scripts', 'new_scripts' );
+
